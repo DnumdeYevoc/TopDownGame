f89c33ea8bb3a16920d7b28bd1183c869b47964f
DnumdeYevoc <163893629+DnumdeYevoc@users.noreply.github.com>
2025-09-04 13:50:52 -0400

    Added AutoTiling

--- topdowngame/tile_map_layer.gd
+++ topdowngame/tile_map_layer.gd
@@ -1,23 +1,27 @@
 @tool
 extends TileMapLayer
+
+var random = RandomNumberGenerator.new()
+
+@export var world_seed := 0
+@export var noise = FastNoiseLite.new()
+@export var threshold :int= 10
+@export var load_range = Vector2(100, 100)
+@export var randomise_seed :bool = false:
+	set(new):
+		randomise_seed = new
+		world_seed = random.randi_range(0,99999999)
+		randomise_seed = false
+
 @export var reload :bool = false:
 	set(new):
 		reload = new
 		generate_buildings()
 		reload = false
-@export var world_seed := 0:
-	set(new_seed):
-		world_seed = new_seed
-		generate_buildings()
-		
-@export var noise : FastNoiseLite
-@export var load_range = Vector2(100, 100):
-	set(new_range):
-		load_range = new_range
-		generate_buildings()
 		
 var alt : float
 func _ready() -> void:
+	
 	generate_buildings()
 
 func generate_buildings():
@@ -27,20 +31,29 @@ func generate_buildings():
 		for y in load_range.y:
 			var pos = Vector2(x,y)
 			alt = round(noise.get_noise_2dv(pos)*100.0)
-			if alt > 10:
+			if alt > threshold:
 				set_cell(pos, 0, Vector2(1,1))
 				
 	for x in load_range.x:
 		for y in load_range.y:
-			pass
-			'''if alt > 1:
-				var surrounding : Array[int] = [get_cell_alternative_tile(pos+Vector2i(1,1))*10), 
-											round(noise.get_noise_2dv(pos+Vector2i(-1,1))*10), 
-											round(noise.get_noise_2dv(pos+Vector2i(1,-1))*10), 
-											round(noise.get_noise_2dv(pos+Vector2i(-1,-1))*10)]
+			#if it exists
+			var pos = Vector2i(x,y)
+			var cell_id = get_cell_source_id(pos)
+			if cell_id != -1:
+				var surrounding : Array[int] = [get_cell_source_id(pos+Vector2i(0,1)), 
+											get_cell_source_id(pos+Vector2i(0,-1)), 
+											get_cell_source_id(pos+Vector2i(1,0)), 
+											get_cell_source_id(pos+Vector2i(-1,0))]
 				#make atlas
 				var atlas = Vector2i(1,1)
-				if surrounding[0] != alt or surrounding[1] != alt or surrounding[2] != alt or surrounding[3] != alt:
-					if 
-	
-				'''
+				if surrounding[0] != cell_id or surrounding[1] != cell_id or surrounding[2] != cell_id or surrounding[3] != cell_id:
+					#up
+					if surrounding[0] != cell_id:
+						atlas += Vector2i(0,1)
+					if surrounding[1] != cell_id:
+						atlas += Vector2i(0,-1)
+					if surrounding[2] != cell_id:
+						atlas += Vector2i(1,0)
+					if surrounding[3] != cell_id:
+						atlas += Vector2i(-1,0)
+				set_cell(pos, cell_id, atlas)
