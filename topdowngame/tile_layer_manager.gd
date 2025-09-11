756d5ff6e45bf60854a4b3d45d3aa531302d7e32
DnumdeYevoc <163893629+DnumdeYevoc@users.noreply.github.com>
2025-09-11 15:15:28 -0400

    started revamping chunk system, not working yet

--- topdowngame/tile_layer_manager.gd
+++ topdowngame/tile_layer_manager.gd
@@ -1,46 +1,42 @@
-@tool
+
 extends Node2D
-const world_layer_scene = preload("res://world_layer.tscn")
+const chunk_scene = preload("res://world_layer.tscn")
 var random = RandomNumberGenerator.new()
 
 @onready var amount = 5
 @export var threshold_rate := 20
-@export var render_dis = Vector2i(100, 75)
+@export var render_dis = 3
 @export var noise = FastNoiseLite.new()
 @export var world_seed := 0
-@export var refresh_dis = 10
+@export var chunk_size := 10
 @onready var player =$"../Player"
 var index = 0
-
-@export var randomise_seed :bool = false:
-	set(new):
-		randomise_seed = new
-		world_seed = random.randi_range(0,99999999)
-		noise.seed = world_seed
-		randomise_seed = false
-@export var reload :bool = false:
-	set(new):
-		reload = new
-		generate_world()
-		reload = false
 
 func _ready() -> void:
 	noise.seed = world_seed
 	noise.frequency = 0.01
 	noise.fractal_type = FastNoiseLite.FRACTAL_RIDGED
 	noise.fractal_weighted_strength = 1
-	generate_world()
-func generate_world():
+	generate_start_chunks()
+	
+func generate_start_chunks() -> void:
+	for x in range(render_dis):
+		for y in range(render_dis):
+			create_chunk(Vector2(x,y))
+			
+func create_chunk(pos : Vector2i) -> void:
 	index = 0
 	for layer in amount:
-		add_child(world_layer_scene.instantiate())
-	for child in get_children():
-		child.noise = noise
-		child.player = player
-		child.refresh_dis = refresh_dis
-		child.ref_point = player.position
-		child.render_dis = render_dis
-		child.player = player
-		child.threshold = threshold_rate*index
-		child.generate_tiles(render_dis)
+		var instance = chunk_scene.instantiate()
+		instance.player = player
+		instance.noise = noise
+		instance.render_dis = render_dis
+		instance.chunk_size = chunk_size
+		instance.threshold = threshold_rate*index
+		var x = (pos.x-render_dis/2.0)*chunk_size
+		var y = (pos.y-render_dis/2.0)*chunk_size
+		instance.global_position = Vector2(x,y)
+		add_child(instance)
 		index += 1
+
+		
--- topdowngame/tile_map_layer.gd
+++ topdowngame/tile_map_layer.gd
@@ -1,78 +1,55 @@
 @tool
-extends TileMapLayer
 
-@onready var player : Node2D
+extends TileMapLayer
+var random = RandomNumberGenerator.new()
+
+@onready var player : CharacterBody2D
 @export var noise : FastNoiseLite
 @export var threshold :int
-@export var render_dis : Vector2i
-@export var refresh_dis : int
-@export var height := 10
+@export var render_dis : int
+@onready var parent = get_parent()
+var chunk_size = 32
+var generating := false
 var alt : float
-var ref_point = Vector2i(0,0)
-	
-func _physics_process(delta: float) -> void:
-	update_tiles(render_dis)
-	#if player is on top of you
+@export var randomise_seed :bool = false:
+	set(new):
+		randomise_seed = new
+		noise.seed=  random.randi_range(0,99999999)
+		randomise_seed = false
+@export var reload :bool = false:
+	set(new):
+		reload = new
+		update_tiles(chunk_size)
+		reload = false
 
-func generate_tiles(size):
-	clear()
-	for x in size.x:
-		for y in size.y:
-			#make different so that player doesnt lag behind
-			var pos = Vector2(int(player.position.x)/32-size.x/2+x,int(player.position.y)/32-size.y/2+y)
-			alt = round(noise.get_noise_2dv(pos)*100.0)
-			if alt > threshold:
-				set_cell(pos, 0, Vector2(2,1))
-				
-	for x in size.x:
-		for y in size.y:
-			#if its not being layered over
-			#if it exists
-			var pos = Vector2i(int(player.position.x)/32-size.x/2+x,int(player.position.y)/32-size.y/2+y)
-			var cell_id = get_cell_source_id(pos)
-			if cell_id != -1:
-				var surrounding : Array[int] = [get_cell_source_id(pos+Vector2i(0,1)), 
-											get_cell_source_id(pos+Vector2i(0,-1)), 
-											get_cell_source_id(pos+Vector2i(1,0)), 
-											get_cell_source_id(pos+Vector2i(-1,0))]
-				#make atlas
-				var atlas = Vector2i(2,1)
-				if surrounding[0] == cell_id:
-					atlas.y += 2
-				if surrounding[1] == cell_id:
-					atlas.y += -1
-				if surrounding[2] == cell_id:
-					atlas.x += 1
-				if surrounding[3] == cell_id:
-					atlas.x += -2
-				set_cell(pos, cell_id, atlas)
+func _ready() -> void:
+	update_tiles(chunk_size)
+
 func update_tiles(size):
-	if int(player.position.distance_to(ref_point))/32> refresh_dis*32:
-		ref_point = player.position
+		generating = true
 		clear()
-		for x in size.x:
-			for y in size.y:
-				var pos = Vector2(int(ref_point.x)/32-size.x/2+x,int(ref_point.y)/32-size.y/2+y)
+		for x in size:
+			for y in size:
+				var pos = Vector2(x,y)+ position
 				#if its not already loaded
 				if get_cell_source_id(pos) != 1:
 					alt = round(noise.get_noise_2dv(pos)*100.0)
 					if alt > threshold:
 						set_cell(pos, 0, Vector2(1,2))
-		for x in size.x:
-			for y in size.y:
+		for x in size:
+			for y in size:
 				#if its not being layered over
 				#if it exists
-				var pos = Vector2i(int(ref_point.x)/32-size.x/2+x,int(ref_point.y)/32-size.y/2+y)
-				var cell_id = get_cell_source_id(pos)
-				if cell_id != -1:
-					var surrounding : Array[int] = [get_cell_source_id(pos+Vector2i(0,1)), 
-												get_cell_source_id(pos+Vector2i(0,-1)), 
-												get_cell_source_id(pos+Vector2i(1,0)), 
-												get_cell_source_id(pos+Vector2i(-1,0))]
+					var pos = Vector2(x,y)+ position
+					var cell_id = get_cell_source_id(pos)
+					var surrounding : Array[int] = [get_cell_source_id(pos+Vector2(0,1)), 
+												get_cell_source_id(pos+Vector2(0,-1)), 
+												get_cell_source_id(pos+Vector2(1,0)), 
+												get_cell_source_id(pos+Vector2(-1,0))]
 					#autotiling
 					#if its an edge tile
 					if surrounding[0] != cell_id or surrounding[1] != cell_id or surrounding[2] != cell_id or surrounding[3] != cell_id:
-						var atlas = Vector2i(2,1)
+						var atlas = Vector2(2,1)
 						if surrounding[0] == cell_id:
 							atlas.y += 2
 						if surrounding[1] == cell_id:
@@ -82,4 +59,28 @@ func update_tiles(size):
 						if surrounding[3] == cell_id:
 							atlas.x += -2
 						set_cell(pos, cell_id, atlas)
+		generating = false
 			
+func _process(delta: float) -> void:
+	if generating:
+		return
+	var dist = float(parent.render_dis) / 2.0
+	var player_chunk_pos = parent.player.position / (chunk_size*32)
+	var chunk_pos = floor(position / (chunk_size*32))
+	var dif = player_chunk_pos - chunk_pos - Vector2(0.5,0.5)
+	var moved := false
+	if dif.x > dist:
+		position.x += parent.render_dis * chunk_size*32
+		moved = true
+	elif dif.x < -dist:
+		position.x -= parent.render_dis * chunk_size*32
+		moved = true
+	if dif.y > dist:
+		position.y += parent.render_dis * chunk_size*32
+		moved = true
+	elif dif.y < -dist:
+		position.y -= parent.render_dis * chunk_size*32
+		moved = true
+		
+	if moved:
+		update_tiles(chunk_size)
